2c71e97c482d230738399d685181911e32be2d6a
Jannik <jannikschwarz@outlook.com>
2022-02-24 11:25:33 +0100

    added new lineCounter

--- src/main/LinesOfCodeCounter.java
+++ src/main/LinesOfCodeCounter.java
@@ -7,22 +7,42 @@ import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
 
-public class SemicolonCounter {
+public class LinesOfCodeCounter {
 
-    public long countSemicolons(List<File> files) {
+    public long countLinesOfCode(List<File> files) {
         return files.stream()
-                .map(countSemicolonsInFile)
+                .map(countCodeLines)
                 .reduce(0L, Long::sum);
     }
 
-    private final Function<File, Long> countSemicolonsInFile = file -> {
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
+    private final Function<File, Long> countCodeLines = file -> {
+        long count = 0;
+        count += semicolonsInFile(file);
+        count += startingPeriods(file);
+        return count;
+    };
+
+    private long startingPeriods(File file) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            return reader.lines()
+                    .map(String::trim)
+                    .filter(line -> line.startsWith("."))
+                    .count();
+        }catch (IOException e) {
+            return 0L;
+        }
+    }
+
+    private long semicolonsInFile(File f) {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(f))) {
             return fileReader.lines()
                     .filter(line -> line.endsWith(";"))
                     .count();
         } catch (IOException e) {
             return 0L;
         }
-    };
+    }
+
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class Main {
     // "E:\\projects\\java\\NeuralNetworkVisuals\\src"
-    private static final String filePath = "E:\\projects\\java\\LinesOfCode\\src";
+    private static final String filePath = "E:\\projects\\java\\WorldListFilter\\src\\main";
 
     public static void main(String[] args) {
         List<File> files = new DirectoryTraverser().filesInDirectoryAndSubdirectories(filePath);
-        long amount = new SemicolonCounter().countSemicolons(files);
+        long amount = new LinesOfCodeCounter().countLinesOfCode(files);
         System.out.println("In '" + filePath + "' gibt es ungefähr " + amount + " Zeilen an Code");
     }
 
